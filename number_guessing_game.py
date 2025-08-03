cf02d9e4f37f4ab08c09f45d13cd343025e324fa
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-03 23:34:14 +0530

    add number guessing game with user input and attempt tracking

---
+++ number_guessing_game.py
@@ -0,0 +1,25 @@
+import random 
+
+secret_number = random.randint(1,100)
+
+print('Welcome to the number Guessing Game!')
+print('Enter number between 1 to 100')
+
+attempts = 0
+
+while True:
+    guess = input('Enter your guess: ')
+
+    guess = int(guess)
+
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congrulations you win")
+        break
+
+print(attempts)
