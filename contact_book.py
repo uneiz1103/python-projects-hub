faa079cc325ee548d77aa16e354ea971f8e5c82b
uneiz1103 <uneizshaikh1103@gmail.com>
2025-08-09 22:39:41 +0530

    feat: search contact

--- contact_book.py
+++ contact_book.py
@@ -13,14 +13,25 @@ def add_contact():
 
 def view_contacts():
     if not contacts:
-        print("📭 No contacts found.\n")
+        print("No contacts found.\n")
     else:
-        print("\n📒 Contact List:")
+        print("\n Contact List:")
         for name, info in contacts.items():
             print(f"Name: {name}")
             print(f"Phone: {info['phone']}")
             print(f"Email: {info['email']}")
             print("-" * 20)
 
+def search_contact():
+    name = input("Enter name to search: ").lower()
+    if name in contacts:
+        print(f"\n🔍 Contact found for {name}:")
+        print(f"Phone: {contacts[name]['phone']}")
+        print(f"Email: {contacts[name]['email']}\n")
+    else:
+        print(f"❌ Contact for '{name}' not found.\n")
+
+
 add_contact()
 view_contacts()
+search_contact()
